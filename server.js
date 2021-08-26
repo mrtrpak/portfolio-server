a1fb1224f224e9beafbfb4a89821ed0c70a06e41
Michael Trpak <mrtrpak3@gmail.com>
2021-08-26 14:25:03 -0400

    adding start script and initializing server

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("welcome to server")
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+});
