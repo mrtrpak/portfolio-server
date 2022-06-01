37ae7aeedc06aae131967a35096cb4f6a759e8ce
Michael Trpak <mrtrpak3@gmail.com>
2022-06-01 16:47:11 -0400

    initiating server project

--- server.js
+++ server.js
@@ -8,8 +8,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("welcome to server")
+});
+
+app.get("/soccer", (req, res) => {
+  let code = req.query.code;
+  console.log(req, code, "server check");
+
+  if (process.env.NODE_ENV === 'development') {
+    let { soccerKey }  = require('./secret');
+  } else if (process.env.NODE_ENV === 'production') {
+    let soccerKey = process.env.soccerKey;
+  }
+
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://api.football-data.org/v2/competitions/${code}/standings`,
+    { 
+      method: "GET",  
+      headers: { "X-Auth-Token": soccerKey }
+    }
+  )
 });
 
 app.listen(PORT, () => {
