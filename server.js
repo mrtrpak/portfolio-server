62d7a47c650e469e83afc5620fc52990a954da65
Michael Trpak <mrtrpak3@gmail.com>
2022-06-02 15:47:11 -0400

    setting up server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const App = express();
 const PORT = 3333;
+
+App.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+App.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
