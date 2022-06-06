77598de8ab8940ee114355480224a291d5934b4f
Michael Trpak <mrtrpak3@gmail.com>
2022-06-06 15:05:03 -0400

    updated package.json and file name

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express from "express";
 const App = express();
 const PORT =  3333;
 
-import { soccerKey } from '../secret';
+import { soccerKey } from './secret.js';
 console.log(soccerKey);
 
 App.get("/", (req, res) => {
-  res.send(soccerKey);
+  res.send("hello world");
 });
 
 App.listen(PORT, () => {
