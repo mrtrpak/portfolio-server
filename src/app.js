4eb8abf55f5c49dc6733ac5ee671220639a17b95
Michael Trpak <mrtrpak3@gmail.com>
2022-06-08 15:17:20 -0400

    able to get info from api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const App = express();
-const { soccerApi } = require('./secret');
+
+const { soccerKey } = require('./secret');
+
+
+
+App.get("/", (req, res) => {
+  res.send("hello");
+});
 
 App.get("/soccer", (req, res) => {
-  res.send(soccerApi);
-});
+  const response = axios.get("https://api.football-data.org/v2/competitions/BL1/standings", {
+      headers: {
+        'X-Auth-Token': soccerKey
+      }
+  })
+  .then(res => console.log(res.data))
+  .catch(err => err);
+
+  res.send(response);
+})
 
 module.exports = App;
